34ae0ce739dbb04c4cdbdb424e8faa19d2fb5d96
Catherine Lee <csl@fb.com>
2024-03-11 15:51:12 -0700

    update

--- .jenkins/sphinx_files.py
+++ .jenkins/sphinx_files.py
@@ -7,7 +7,7 @@ def main() -> None:
     env = os.environ.copy()
     for file in files_to_run.split(" "):
         print(f"Running {file}")
-        env["GALLERY_PATTERN"] = file
+        env["RUN_ONLY"] = file
         subprocess.check_output(["make", "html"], env=env)
 
 
